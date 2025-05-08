0e4c0a7c853cd41d57a8208aeb37fd0400733f60
Waleedalhrbi <waleedalhrbi2001@gmail.com>
2025-05-08 14:31:11 +0300

    Abdulraouf Add badge model, service, cntroler (#3)
    * Added my changes
    
    * Badge is added
    
    ---------
    
    Co-authored-by: AbdulraoufKM <imabdulraouf@gmail.com>

---
+++ src/main/java/org/example/atharvolunteeringplatform/Model/Badge.java
@@ -0,0 +1,46 @@
+package org.example.atharvolunteeringplatform.Model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "Title is required")
+    @Size(max = 100, message = "Title must not exceed 100 characters")
+    private String title;
+
+    @NotEmpty(message = "Description is required")
+    @Size(max = 255, message = "Description must not exceed 255 characters")
+    private String description;
+
+    @NotEmpty(message = "Criteria is required")
+    private Integer criteria;
+
+    @NotEmpty(message = "Image path is required")
+    private String imagePath;
+
+
+
+
+    @ManyToMany(mappedBy = "badges")
+    private Set<Student> students;
+
+}
