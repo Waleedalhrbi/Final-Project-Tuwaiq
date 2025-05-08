38d76fc3a5e5815023d2ace694b93f9174cd9625
Waleedalhrbi <waleedalhrbi2001@gmail.com>
2025-05-09 02:03:37 +0300

    Albatool (#4)
    * Add school , School,Complaint
    
    * Add school , School,Complaint
    
    ---------
    
    Co-authored-by: AlbatoolAbdurhman <albatoolabdurhman2001@gmail.com>

--- src/main/java/org/example/atharvolunteeringplatform/Model/Complaint.java
+++ src/main/java/org/example/atharvolunteeringplatform/Model/Complaint.java
@@ -1,6 +1,7 @@
 package org.example.atharvolunteeringplatform.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -24,20 +25,23 @@ public class Complaints {
 
     @Column(nullable = false)
     @NotEmpty(message = "Title cannot be Empty")
+    @Max(value = 30,message = "Title cannot exceed  than 30 characters")
+
     private String title;
 
     @Column(nullable = false)
     @NotEmpty(message = "description cannot be Empty")
+    @Max(value = 300,message = "description cannot exceed  than 300 characters")
     private String description;
 
-    @Pattern(regexp = "In Progress|Resolved|Closed")
+    @Pattern(regexp = "^(In Progress|Resolved|Closed)$")
     private String status;
 
     private LocalDateTime CreateAt;
 
 //    @ManyToOne
 //    private Student students;
-//    @ManyToOne
-//    private Opportuity opportuity;
+    @ManyToOne
+    private Opportunity opportunity ;
 
 }
