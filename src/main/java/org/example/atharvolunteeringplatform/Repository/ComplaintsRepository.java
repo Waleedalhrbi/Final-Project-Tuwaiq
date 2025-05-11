6b89af57ee7b2717c5fde09bf27a495dacfbe3d2
Waleedalhrbi <waleedalhrbi2001@gmail.com>
2025-05-11 11:32:40 +0300

    Waleed (#17)
    * extra endpoints waleed
    
    * solve conflict
    
    * All extra endpoint from waleed
    
    * solve big mistakes

--- src/main/java/org/example/atharvolunteeringplatform/Repository/ComplaintsRepository.java
+++ src/main/java/org/example/atharvolunteeringplatform/Repository/ComplaintsRepository.java
@@ -4,9 +4,11 @@ package org.example.atharvolunteeringplatform.Repository;
 import org.example.atharvolunteeringplatform.Model.Complaint;
 import org.example.atharvolunteeringplatform.Model.Opportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -14,11 +16,19 @@ public interface ComplaintsRepository extends JpaRepository<Complaint, Integer> 
 
     Complaint findComplaintsById(Integer id);
 
-    List<Complaint> findComplaintsByCreateAtDateBetween(LocalDate from, LocalDate to);
 
 
     //12
     List<Complaint> findByStudentsId(Integer studentId);
+
+
+    @Query("SELECT c FROM Complaint c WHERE c.students.id = ?1 AND c.createAt >= ?2 AND c.createAt <= ?3")
+    List<Complaint> findByStudentIdAndCreateAtBetween(Integer studentId, LocalDateTime from, LocalDateTime to);
+
+
+
+    @Query("SELECT c FROM Complaint c WHERE c.students.id = :studentId AND c.status = :status")
+    List<Complaint> findByStudentIdAndStatus(Integer studentId, String status);
 
 
 }
